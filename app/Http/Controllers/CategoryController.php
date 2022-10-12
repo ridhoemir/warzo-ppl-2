e38120e884cd376bdc1148f01515b1437e73c67b
ridhoemir <ridhoemir64@gmail.com>
2022-10-12 12:57:56 +0700

    Get Category for each user(merchant)

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        $data = Category::all();
+        $data = Category::where('user_id', auth()->user()->id)->get();
         return Inertia::render('Category/Index', ['data' => $data]);
         // dd($data);
     }
