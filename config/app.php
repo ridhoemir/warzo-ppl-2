c0475bb3bada240d1a74956d08fb86689227cf0d
Muhammad Faishal Dienul Haq <mhmd.faishal123@gmail.com>
2022-10-12 11:36:28 +0700

    feature/user-authentication-with-roles/v.0.0.1

--- config/app.php
+++ config/app.php
@@ -210,6 +210,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Route' => Illuminate\Support\Facades\Route::class,
     ])->toArray(),
 
 ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Modules\User\Database\Seeders\PermissionsTableSeeder;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,11 +15,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call(PermissionsTableSeeder::class);
+        $this->call(SuperUserSeeder::class);
     }
 }
